63fae8c2d1fd4d37112f83d7f66fac0fd6d0bc89
i-Taozi <879714748@qq.com>
2022-04-06 20:26:34 +0800

    NightscoutFoundation/xDrip:original code

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/webservices/WebResponse.java
@@ -0,0 +1,49 @@
+package com.eveningoutpost.dexdrip.webservices;
+
+import com.eveningoutpost.dexdrip.Models.UserError;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by jamorham on 06/01/2018.
+ *
+ * Data class for webservice responses
+ */
+
+public class WebResponse {
+
+    private static String TAG = "WebResponse";
+
+    byte[] bytes;
+    String mimeType;
+    int resultCode;
+
+    WebResponse(String str) {
+        this(str, 200, "application/json");
+    }
+
+    WebResponse(String str, int resultCode, String mimeType) {
+        try {
+            bytes = str.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            UserError.Log.wtf(TAG, "UTF8 is unsupported!");
+        }
+        this.mimeType = mimeType;
+        this.resultCode = resultCode;
+    }
+
+    public String getResultDesc() {
+        switch (resultCode) {
+            case 200:
+                return "OK";
+            case 400:
+                return "Bad Request";
+            case 404:
+                return "Not Found";
+            case 500:
+                return "Server Error";
+            default:
+                return "Unknown";
+        }
+    }
+}
